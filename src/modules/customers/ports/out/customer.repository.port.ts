bbc29b055bc42194b3b4f48313be43dfd4724609
João Carvalho <jrgc.sp@gmail.com>
2025-06-06 13:45:06 -0300

    fix: remove out rule from gitignore

---
+++ src/modules/customers/ports/out/customer.repository.port.ts
@@ -0,0 +1,13 @@
+import { Customer } from '../../domain/core/customer.entity';
+
+export const CUSTOMER_REPOSITORY = 'CUSTOMER_REPOSITORY';
+
+export interface ICustomerRepository {
+  create(customer: Partial<Customer>): Promise<Customer>;
+  findAll(): Promise<Customer[]>;
+  findOne(id: string): Promise<Customer>;
+  update(id: string, customer: Partial<Customer>): Promise<Customer>;
+  remove(id: string): Promise<Customer>;
+  findByEmail(email: string): Promise<Customer>;
+  findByCpf(cpf: string): Promise<Customer>;
+}
